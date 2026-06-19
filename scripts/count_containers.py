820dd742bde87b231adb2b5dc01a8da31e912981
Jedidiah Chibinga <jedidiahchibinga@gmail.com>
2026-06-19 06:52:47 +0200

    re add std_z filter

--- scripts/count_containers.py
+++ scripts/count_containers.py
@@ -380,11 +380,11 @@ def add_stack_elevation(container_boxes_gdf):
 
 def filter_clean_container_boxes_gdf(container_boxes_gdf):
     container_boxes_gdf['area'] = container_boxes_gdf.area
-    std_z_filter = 5 # to filter out very tall flood lights in image
+    std_z_filter = 8.5 # to filter out very tall flood lights in image
     min_aspect_ratio = 2
     min_stack_height = 2
     
-    # container_boxes_gdf = container_boxes_gdf[(container_boxes_gdf['std_z'] < std_z_filter)]
+    container_boxes_gdf = container_boxes_gdf[(container_boxes_gdf['std_z'] < std_z_filter)]
     
     # container_boxes_gdf = container_boxes_gdf[(container_boxes_gdf['mean_z'] > 51)]
     
@@ -512,30 +512,30 @@ def add_3d_visualisation(container_boxes_gdf):
         initial_view_state=view_state,
         map_style=pdk.map_styles.LIGHT,
         # map_style="mapbox://styles/mapbox/satellite-v9",
-        tooltip = {
-            "html": """
-            <b>Stack height:</b> {stack_height}<br>
-            <b>Containers:</b> {n_containers}
-            """,
-            "style": {"color": "white"},
-        },
-        #  tooltip = {
+        # tooltip = {
         #     "html": """
         #     <b>Stack height:</b> {stack_height}<br>
-        #     <b>Containers:</b> {n_containers}<br>
-        #     <b>mean_z:</b> {mean_z}<br>
-        #     <b>median_z:</b> {median_z}<br>
-        #     <b>dsm_mean:</b> {dsm_mean}<br>
-        #     <b>dsm_median:</b> {dsm_median}<br>
-        #     <b>std_z:</b> {std_z}<br>
-        #     <b>area_px:</b> {area_px}<br>
-        #     <b>area:</b> {area}<br>
-        #     <b>aspect_ratio:</b> {aspect_ratio}<br>
-        #     <b>point_count:</b> {point_count}<br>
-        #     <b>color:</b> {color}
+        #     <b>Containers:</b> {n_containers}
         #     """,
         #     "style": {"color": "white"},
         # },
+         tooltip = {
+            "html": """
+            <b>Stack height:</b> {stack_height}<br>
+            <b>Containers:</b> {n_containers}<br>
+            <b>mean_z:</b> {mean_z}<br>
+            <b>median_z:</b> {median_z}<br>
+            <b>dsm_mean:</b> {dsm_mean}<br>
+            <b>dsm_median:</b> {dsm_median}<br>
+            <b>std_z:</b> {std_z}<br>
+            <b>area_px:</b> {area_px}<br>
+            <b>area:</b> {area}<br>
+            <b>aspect_ratio:</b> {aspect_ratio}<br>
+            <b>point_count:</b> {point_count}<br>
+            <b>color:</b> {color}
+            """,
+            "style": {"color": "white"},
+        },
     )
     
     # deck.to_html("test.html")
